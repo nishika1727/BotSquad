4ec0882e5ee59a48bf6d06f54a66815b680c1005
Nikhil Kaundal <120103802+nikhil1234567890123@users.noreply.github.com>
2025-07-16 16:34:01 +0530

    Add files via upload

---
+++ backend/src/Final Backend/intent_links.py
@@ -0,0 +1,30 @@
+intent_to_url = {
+    "admission": {
+        "keywords": ["admission", "apply", "entrance", "how to get admission"],
+        "urls": ["https://admissions.puchd.ac.in/"]
+    },
+    "exam": {
+        "keywords": ["exam", "result", "datesheet", "marksheet"],
+        "urls": ['https://exams.puchd.ac.in/datesheet.php', 'https://results.puexam.in/']
+    },
+    "fee": {
+        "keywords": ["fee", "fees", "payment", "pay"],
+        "urls": ['https://payonline.puchd.ac.in/']
+    },
+    "hostel": {
+        "keywords": ["hostel", "accommodation", "rooms"],
+        "urls": ['https://hostels.puchd.ac.in/']
+    },
+    "placement": {
+        "keywords": ["placement", "jobs", "career"],
+        "urls": ['https://cpc.puchd.ac.in/']
+    },
+    "syllabus": {
+        "keywords": ["syllabus", "subjects"],
+        "urls": ['https://puchd.ac.in/syllabus.php']
+    },
+    "general": {
+        "keywords": [],
+        "urls": ['https://puchd.ac.in/']
+    }
+}
